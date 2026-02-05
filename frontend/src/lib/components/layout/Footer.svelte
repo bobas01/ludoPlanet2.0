00f4377d16737fd0701c568c6388d6e0282e7b21
bobas01 <alexandre.sequeira01@gmail.com>
2026-02-05 14:56:10 +0100

    add legal information in the footer

--- frontend/src/lib/components/layout/Footer.svelte
+++ frontend/src/lib/components/layout/Footer.svelte
@@ -5,6 +5,11 @@
 	import iconPhone from '$lib/assets/icons/iconPhone.png';
 	import iconsInsta from '$lib/assets/icons/iconsInsta.png';
 	import iconTwitter from '$lib/assets/icons/iconTwitter.png';
+
+	import mentionsLegales from '$lib/assets/documents/Mentions Légales Ludo Planet.pdf';
+	import cgv from '$lib/assets/documents/CGV Ludo Planet.pdf';
+	import politiqueConfidentialite from '$lib/assets/documents/Politique Confidentialite.pdf';
+	import politiqueCookies from '$lib/assets/documents/Politique Cookies Ludo.pdf';
 </script>
 
 <footer class="bg-cover bg-center p-10" style={`background-image: url(${bgHeader});`}>
@@ -16,6 +21,7 @@
 				<p class="text-balance">Sauf soirée jeux: Fermeture à minuit</p>
 			</div>
 		</section>
+
 		<section class="flex flex-col">
 			<p class="mb-4 text-lg font-bold text-[var(--brand-accent)]">Contact</p>
 			<div class="flex flex-col gap-1">
@@ -29,18 +35,35 @@
 				</div>
 			</div>
 		</section>
+
 		<section class="flex flex-col">
 			<p class="mb-4 text-lg font-bold text-[var(--brand-accent)]">Suivez-nous</p>
 			<div class="flex flex-wrap items-center gap-4">
-				<a href="https://www.facebook.com/profile.php?id=100089942604311" aria-label="Facebook"
-					><img src={iconFb} alt="Facebook" class="h-10 w-10" /></a
-				>
-				<a href="https://www.instagram.com/jeux_de_societe_dezaim/" aria-label="Instagram"
-					><img src={iconsInsta} alt="Instagram" class="h-10 w-10" /></a
-				>
-				<a href="https://x.com/JeuxDeSocieteDezaim" aria-label="Twitter"
-					><img src={iconTwitter} alt="Twitter" class="h-10 w-10" /></a
-				>
+				<a href="https://www.facebook.com/profile.php?id=100089942604311" aria-label="Facebook">
+					<img src={iconFb} alt="Facebook" class="h-10 w-10" />
+				</a>
+				<a href="https://www.instagram.com/jeux_de_societe_dezaim/" aria-label="Instagram">
+					<img src={iconsInsta} alt="Instagram" class="h-10 w-10" />
+				</a>
+				<a href="https://x.com/JeuxDeSocieteDezaim" aria-label="Twitter">
+					<img src={iconTwitter} alt="Twitter" class="h-10 w-10" />
+				</a>
+			</div>
+		</section>
+
+		<section class="flex flex-col">
+			<p class="mb-4 text-lg font-bold text-[var(--brand-accent)]">Informations légales</p>
+			<div class="flex flex-col gap-1 text-sm">
+				<a href={mentionsLegales} target="_blank" rel="noopener noreferrer"> Mentions légales </a>
+				<a href={cgv} target="_blank" rel="noopener noreferrer">
+					Conditions générales de vente (CGV)
+				</a>
+				<a href={politiqueConfidentialite} target="_blank" rel="noopener noreferrer">
+					Politique de confidentialité
+				</a>
+				<a href={politiqueCookies} target="_blank" rel="noopener noreferrer">
+					Politique de cookies
+				</a>
 			</div>
 		</section>
 	</section>
