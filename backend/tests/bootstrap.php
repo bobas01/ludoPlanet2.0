6978451d048191d7d1649e952cd27ffafaf5d9d5
bobas01 <alexandre.sequeira01@gmail.com>
2026-02-13 19:07:05 +0100

    change bootsdtrap

--- backend/tests/bootstrap.php
+++ backend/tests/bootstrap.php
@@ -2,11 +2,13 @@
 
 use Symfony\Component\Dotenv\Dotenv;
 
-require dirname(__DIR__).'/vendor/autoload.php';
+require dirname(__DIR__) . '/vendor/autoload.php';
 
-if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
-}
+// if (method_exists(Dotenv::class, 'bootEnv')) {
+//     (new Dotenv())->bootEnv(dirname(__DIR__).'/.env');
+// }
+$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? 'prod';
+$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] ?? $_SERVER['APP_DEBUG'] ?? '0';
 
 if ($_SERVER['APP_DEBUG']) {
     umask(0000);
