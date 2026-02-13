e7ce861980f398516e6f080bc2ca281c8873401d
bobas01 <alexandre.sequeira01@gmail.com>
2026-02-13 19:16:35 +0100

    delet and remake original bootstrap file

--- backend/tests/bootstrap.php
+++ backend/tests/bootstrap.php
@@ -4,15 +4,8 @@ use Symfony\Component\Dotenv\Dotenv;
 
 require dirname(__DIR__) . '/vendor/autoload.php';
 
-// Désactive le chargement du .env et force l'utilisation des variables du conteneur
-$_SERVER['APP_ENV'] = $_ENV['APP_ENV'] ?? $_SERVER['APP_ENV'] ?? 'prod';
-$_SERVER['APP_DEBUG'] = $_ENV['APP_DEBUG'] ?? $_SERVER['APP_DEBUG'] ?? '0';
-putenv('APP_ENV=' . $_SERVER['APP_ENV']);
-putenv('APP_DEBUG=' . $_SERVER['APP_DEBUG']);
-// Empêche Symfony de chercher un .env
 if (method_exists(Dotenv::class, 'bootEnv')) {
-    (new Dotenv())->usePutenv(false);
-    // (new Dotenv())->bootEnv(dirname(DIR).'/.env'); // Désactivé
+    (new Dotenv())->bootEnv(dirname(__DIR__) . '/.env');
 }
 
 if ($_SERVER['APP_DEBUG']) {
